e4ce50eb05716fa4cc710081eb28599eb29bec37
Jason Kimm <jasondkimm@proton.me>
2025-01-11 13:29:58 -0600

    Add files via upload
    Updated flake with correct input management for nixvim

--- flake.nix
+++ flake.nix
@@ -2,50 +2,43 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    # nix colors
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nvf.url = "github:notashelf/nvf";
     nix-colors.url = "github:misterio77/nix-colors";
-    
-    # nixvim
-    nixvim.url = "github:dc-tec/nixvim";
-
-    # stylix
+    nixvim.url = "github:nix-community/nixvim";
     stylix.url = "github:danth/stylix/release-24.05";
-
-    # nix index database
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-
-    # NUR
     nur.url = "github:nix-community/NUR";
-
-    # home-manager, used for managing user configuration
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, nix-index-database, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, nix-index-database, nix-colors, stylix, nvf, nur, nixvim, ... }: {
+
+    # packages."x86_64-linux".default =
+    #   (nvf.lib.neovimConfiguration {
+    #     pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    #     modules = [ ./nvf-configuration.nix ];
+    #   }).neovim;
+
     nixosConfigurations = {
       # TODO please change the hostname to your own
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        # extraSpecialArgs = { inherit inputs; };
+        specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
           nix-index-database.nixosModules.nix-index
           home-manager.nixosModules.home-manager
+          nvf.nixosModules.default
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.backupFileExtension = "hm-backup";
+            home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.jason = import ./home.nix;
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
